44f37f87a80330d01840734acb341b9709a05e26
TUKich <TUK.eSports@yandex.ru>
2021-12-09 06:39:34 +0300

    C тебя энер сегодня

---
+++ Лаба Юрка/L10/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Linq;
+
+namespace Лаба_Юрка
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                BinaryNumber bNum1;
+                BinaryNumber bNum2;
+                BinaryNumber bNum3;
+                BinaryNumber bNum4;
+
+                //Обработка исключений
+                try
+                {
+                    bNum1 = new BinaryNumber("10011");
+                    bNum2 = new BinaryNumber("0011");
+                    bNum3 = new BinaryNumber("10101");
+                    bNum4 = new BinaryNumber("0011");
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.ReadLine();
+                    return;
+                }
+
+                //Сложение
+                BinaryNumber bNumRes1 = bNum1 + bNum2;
+                Console.WriteLine(bNumRes1);
+
+                //Вычитание
+                BinaryNumber bNumRes2 = bNum3 - bNum4;
+                Console.WriteLine(bNumRes2);
+
+                //Умножение
+                BinaryNumber bNumRes3 = bNum1 * bNum2;
+                Console.WriteLine(bNumRes3);
+
+                //Деление
+                BinaryNumber bNumRes4 = bNum3 / bNum4;
+                Console.WriteLine(bNumRes4);
+
+                //Сравнение
+                Console.WriteLine(bNum2.Equals(bNum4));
+                Console.ReadLine();
+            }
+        }
+
+        public class BinaryNumber
+        {
+            //Внутренее значение числа
+            private int innerNumber;
+
+            //Конструктор с проверкой формата параметра
+            public BinaryNumber(string bNumber)
+            {
+                bool res = bNumber.ToCharArray().Any(n => (n != '0' && n != '1'));
+                if (res)
+                    throw new FormatException("Неверный формат данных!");
+
+                innerNumber = Convert.ToInt32(bNumber, 2);
+            }
+
+            //Перегрузка оператора +
+            public static BinaryNumber operator +(BinaryNumber bn1, BinaryNumber bn2)
+            {
+                int b1 = Convert.ToInt32(bn1.ToString(), 2);
+                int b2 = Convert.ToInt32(bn2.ToString(), 2);
+                return new BinaryNumber(Convert.ToString(b1 + b2, 2));
+            }
+
+            //Перегрузка оператора -
+            public static BinaryNumber operator -(BinaryNumber bn1, BinaryNumber bn2)
+            {
+                int b1 = Convert.ToInt32(bn1.ToString(), 2);
+                int b2 = Convert.ToInt32(bn2.ToString(), 2);
+                return new BinaryNumber(Convert.ToString(b1 - b2, 2));
+            }
+
+            //Перегрузка оператора *
+            public static BinaryNumber operator *(BinaryNumber bn1, BinaryNumber bn2)
+            {
+                int b1 = Convert.ToInt32(bn1.ToString(), 2);
+                int b2 = Convert.ToInt32(bn2.ToString(), 2);
+                return new BinaryNumber(Convert.ToString(b1 * b2, 2));
+            }
+
+            //Перегрузка оператора /
+            public static BinaryNumber operator /(BinaryNumber bn1, BinaryNumber bn2)
+            {
+                int b1 = Convert.ToInt32(bn1.ToString(), 2);
+                int b2 = Convert.ToInt32(bn2.ToString(), 2);
+                return new BinaryNumber(Convert.ToString(b1 / b2, 2));
+            }
+
+            //Перегрузка Equals
+            public override bool Equals(object obj)
+            {
+                if (obj is BinaryNumber)
+                {
+                    if (this.ToString() == ((BinaryNumber) obj).ToString())
+                        return true;
+                }
+
+                return false;
+            }
+
+            //Перегрузка GetHashCode
+            public override int GetHashCode()
+            {
+                return this.ToString().GetHashCode();
+            }
+
+            //Перегрузка ToString()
+            public override string ToString()
+            {
+                return Convert.ToString(innerNumber, 2);
+            }
+        }
+    }
+}
---
+++ Лаба Юрка/L11/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace L11
+{
+    class Program
+    {
+        private double a;
+        private double b;
+        public double A
+        {
+            get
+            {
+                return this.a;
+            }
+            set
+            {
+                this.a = value;
+            }
+            
+        }
+
+        public  double B
+        {
+            get
+            {
+                return this.b;
+            }
+            set
+            {
+                this.b = value;
+            }
+        }
+        
+
+        public static double Min(double a, double b)
+        {
+            if (a > b)
+                return b;
+            else
+                return a;
+        }
+        static void Main(string[] args)
+        {
+            Program bi = new Program();
+            bi.A = Convert.ToDouble(Console.ReadLine());
+            bi.B = Convert.ToDouble(Console.ReadLine());
+            double u = Min(bi.A, bi.B);
+            double v = Min(bi.A * bi.B, bi.B) * Min(u + Math.Pow(bi.A, 2), Math.PI);
+            Console.WriteLine(u+"\n"+v);
+        }
+    }
+}
